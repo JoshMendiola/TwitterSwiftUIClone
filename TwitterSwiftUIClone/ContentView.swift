754af234d3057f07a00b9f8c9d916f656a713d0a
Ryan Aparicio <aparicioryan2003@gmail.com>
2023-01-30 21:03:47 -0600

    Initial Commit

---
+++ TwitterSwiftUIClone/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TwitterSwiftUIClone
+//
+//  Created by Ryan Aparicio on 1/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TwitterSwiftUIClone/TwitterSwiftUICloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUICloneApp.swift
+//  TwitterSwiftUIClone
+//
+//  Created by Ryan Aparicio on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUICloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
